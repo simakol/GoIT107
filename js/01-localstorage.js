6a38ff343c28287103f614603eb254bd34491f15
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-07-09 21:35:01 +0300

    lesson-17

--- js/01-localstorage.js
+++ js/01-localstorage.js
@@ -10,14 +10,67 @@ const names = ["Alice", "Kate", "Emma"];
  * Чому треба використовувати метод JSON.stringify
  */
 
+// localStorage.setItem("user id", "1")
+//* localStorage.setItem(key, value) - записує дані (value) в локальний сховок по ключу key
+
+//* JSON.stringify(value) - бере значення value і намагається перетворити його на JSON формат
+
+localStorage.setItem(LS_KEY, JSON.stringify(names));
+
 /**
  * Читання
  * Чому треба використовувати метод JSON.parse
  */
 
+//* localStorage.getItem(key) - зчитати дані які записані в локальному сховку по ключу key
+
+// console.log(localStorage.getItem("smth")); // якщо ми хочемо зчитати дані по неіснуючому ключу - ми отримаємо null
+
+const namesFromLS = localStorage.getItem(LS_KEY);
+
+console.log(namesFromLS);
+console.log(JSON.parse(namesFromLS));
+// * JSON.parse(validJSON) - приймає валідний JSON та перетворює його на валідний JS обʼєкт(масив)
+
+console.log("Починаємо парсинг не валідного ДЖСОН");
+
+/*
+JS
+{
+  user: "Alex",
+  age: 20,
+  stack: ["html", "css", "js", "react"],
+  isOnline: true
+}
+
+JSON
+'{
+  "user": "Alex",
+  "age": 20,
+  "stack": ["html", "css", "js", "react"],
+  "isOnline": true
+}'
+
+*/
+
+try {
+  // блок трай, у якому ви описуєте код який потенційно може викинути помилку
+  console.log(JSON.parse('{ "name": "Alex" }'));
+  console.log(JSON.parse('["Alex", "Rick"]'));
+  console.log(JSON.parse("sdfsdfsdf"));
+} catch (err) {
+  // блок кетч, його задача - зловити помилку яка виникла в блоці трай та обробити її
+  console.log("ERROR:", err);
+}
+
+console.log("Закінчуємо парсинг не валідного ДЖСОН");
+
 /**
  * Видалення
  */
+//* localStorage.removeItem(key) - приймає ключ та видаляє всю інформацію зі сховища по цьому ключу key
+
+localStorage.removeItem(LS_KEY);
 
 /**
  * LocalStorage не може зберігати функції
@@ -27,6 +80,9 @@ function add(a, b) {
   return a + b;
 }
 
+localStorage.setItem("my function", JSON.stringify(add)); // undefined
+console.log(localStorage.getItem("my function")); // undefined
+
 const calculator = {
   a: 5,
   b: 10,
@@ -34,3 +90,17 @@ const calculator = {
     return this.a + this.b;
   },
 };
+
+/*
+JSON.stringify(calculator) ->
+'{
+  "a": 5,
+  "b": 10
+}'
+*/
+
+localStorage.setItem("calculator", JSON.stringify(calculator));
+
+console.log(localStorage.getItem("calculator"));
+
+console.log(JSON.parse(localStorage.getItem("calculator")));
--- js/02-feedback.js
+++ js/02-feedback.js
@@ -1,7 +1,11 @@
-const STORAGE_KEY = "feedback-msg";
+const STORAGE_KEY = "feedback-form";
 
 const form = document.querySelector(".feedback-form");
-const textarea = form.querySelector("textarea");
+
+populateForm();
+
+form.addEventListener("submit", handleFormSubmit);
+form.addEventListener("input", handleFormInput);
 
 /*
  * - Скасовуємо стандартну поведінку
@@ -9,12 +13,71 @@ const textarea = form.querySelector("textarea");
  * - Очищуємо форму
  */
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  event.currentTarget.reset();
+}
+
 /*
  * - Отримуємо значення поля
  * - Зберігаємо його у сховище
  */
 
+function handleFormInput(event) {
+  const value = event.target.value;
+  const key = event.target.name;
+
+  let savedFeedbackData = {};
+
+  try {
+    savedFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  // якщо в ЛС існують дані (обʼєкт), то ми звертаємось вже до існуючого обʼєкту, та змінюмо/створюємо поля зі значеннями
+  if (savedFeedbackData) {
+    savedFeedbackData[key] = value;
+  } else {
+    // якщо в ЛС немає взагалі даних (обʼєкту), то ми його самостійно створюємо та записуємо туди перший ключ з значенням
+    savedFeedbackData = {
+      [key]: value,
+    };
+  }
+
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(savedFeedbackData));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
 /*
  * - Отримуємо значення зі сховища
  * - Якщо там щось було, оновлюємо DOM
  */
+
+function populateForm() {
+  let savedFeedbackData = {};
+
+  try {
+    savedFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+
+  // якщо в ЛС не існує таке значення, то виходимо з фукнції
+  if (!savedFeedbackData) {
+    return;
+  }
+
+  for (const key in savedFeedbackData) {
+    form.elements[key].value = savedFeedbackData[key];
+  }
+}
