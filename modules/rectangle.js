7d89390e29f07917c9a1801fa2bc69a9baff14d6
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-07-11 20:55:55 +0300

    lesson-18

---
+++ modules/rectangle.js
@@ -0,0 +1,14 @@
+//! default export - експорт за замовчуванням може бути ТІЛЬКИ ОДИН на весь файл
+//* v1
+
+// export default function calculateArea(a, b) {
+//   return a * b;
+// }
+
+//*v2
+
+function calculateArea(a, b) {
+  return a * b;
+}
+
+export default calculateArea;
