4522ce5630e2534ca9f45973d3a137faa381034e
Alexey Simak <40754302+simakol@users.noreply.github.com>
2024-07-18 21:56:04 +0300

    lesson-20

--- js/02-promisification.js
+++ js/02-promisification.js
@@ -4,29 +4,55 @@
  * - Функція, яка повертає проміс
  */
 
-const makeOrder = (dish, onSuccess, onError) => {
-  const passed = Math.random() > 0.5;
+//! without promises
+// const makeOrder = (dish, onSuccess, onError) => {
+//   const passed = Math.random() > 0.5;
 
-  setTimeout(() => {
-    if (passed) {
-      onSuccess(`✅ Ваше замовлення: ${dish}`);
-    }
+//   setTimeout(() => {
+//     if (passed) {
+//       onSuccess(`✅ Ваше замовлення: ${dish}`);
+//     }
 
-    onError("❌ Упс, у нас закінчилися продукти");
-  }, 1000);
+//     onError("❌ Упс, у нас закінчилися продукти");
+//   }, 1000);
+// };
+
+// makeOrder(
+//   "пиріжок",
+//   (result) => {
+//     console.log("onMakeOrderSuccess");
+//     console.log(result);
+//   },
+//   (error) => {
+//     console.log("onMakeOrderError");
+//     console.log(error);
+//   }
+// );
+
+//* with promises
+const makeOrder = (dish) => {
+  return new Promise((res, rej) => {
+    const passed = Math.random() > 0.5;
+
+    setTimeout(() => {
+      if (passed) {
+        res(`✅ Ваше замовлення: ${dish}`);
+      }
+
+      rej("❌ Упс, у нас закінчилися продукти");
+    }, 1000);
+  });
 };
 
-makeOrder(
-  "пиріжок",
-  (result) => {
-    console.log("onMakeOrderSuccess");
-    console.log(result);
-  },
-  (error) => {
-    console.log("onMakeOrderError");
-    console.log(error);
-  }
-);
+// makeOrder("пиріжок")
+//   .then((result) => {
+//     console.log("onMakeOrderSuccess");
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log("onMakeOrderError");
+//     console.log(error);
+//   });
 
 /*
  * Промісифікація «синхронних» функцій
@@ -34,24 +60,47 @@ makeOrder(
  * - Promise.reject()
  */
 
-const prepareDish = (dish, onSuccess, onError) => {
+//! without promises
+// const prepareDish = (dish, onSuccess, onError) => {
+//   const passed = Math.random() > 0.5;
+
+//   if (passed) {
+//     onSuccess(`✅ Ваше замовлення: ${dish}`);
+//   }
+
+//   onError("❌ Упс, у нас закінчилися продукти");
+// };
+
+// prepareDish(
+//   "пиріжок",
+//   (result) => {
+//     console.log("onMakeOrderSuccess");
+//     console.log(result);
+//   },
+//   (error) => {
+//     console.log("onMakeOrderError");
+//     console.log(error);
+//   }
+// );
+
+//* with promises
+
+const prepareDish = (dish) => {
   const passed = Math.random() > 0.5;
 
-  if (passed) {
-    onSuccess(`✅ Ваше замовлення: ${dish}`);
-  }
-
-  onError("❌ Упс, у нас закінчилися продукти");
+  return passed
+    ? Promise.resolve(`✅ Ваше замовлення: ${dish}`)
+    : Promise.reject("❌ Упс, у нас закінчилися продукти");
 };
 
-makeOrder(
-  "пиріжок",
-  (result) => {
+// Promise.resolve і Promise.reject - створюють проміс на ходу та відразу переведуть його у стан Fullfilled або Rejected відповідно. В цих статичних методах немає фукнції executor, в якості аргументів вони приймають відразу дані, які далі будуть оброблюватись в then або в catch
+
+prepareDish("пиріжок")
+  .then((result) => {
     console.log("onMakeOrderSuccess");
     console.log(result);
-  },
-  (error) => {
+  })
+  .catch((error) => {
     console.log("onMakeOrderError");
     console.log(error);
-  }
-);
+  });
